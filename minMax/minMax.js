ed42d5307df99da2495179375ffbd5f1ba3bc95f
Toyeen <rightstiches@yahoo.com>
2016-11-27 13:25:29 +0100

    Add files via upload
    Update to Tests

---
+++ minMax/minMax.js
@@ -0,0 +1,20 @@
+'use strict';
+
+    minmax: function(arr[n]){
+    max=max(arr[n]);
+    min=min(arr[n]);
+        if (arr[0])=min && arr[n-1]=max){
+            return '[min, max]';
+        }elseif (arr.length=2){
+            return '[min, max]';
+        }elseif(min=max){
+             return '[min]';   
+        }else{
+            return'[min, max]';
+        }
+    }
+
+
+
+
+   
