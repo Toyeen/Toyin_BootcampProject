473b93367daed1f57eb6ed5f17b12d0fd0a582a7
Toyeen <rightstiches@yahoo.com>
2016-11-28 16:09:19 +0100

    Add files via upload

---
+++ Car/Car.js
@@ -0,0 +1,23 @@
+
+var  Car = function(){
+
+var car = newCar(brand, model, name);
+
+    if (typeof (car) === 'object' && instanceof (Car) = car;
+   
+      return  honda = new Car ('Honda');
+      
+    else if (car) = newCar();
+    
+      return  var gm = new(Car);  
+                  gm.name == 'General';
+                  gm,model == 'GM';
+      
+    else if car = [name, model]
+    
+       return var toyota = new Car('Toyota','Corolla');
+               
+    else if var Opel = new('Opel','Omega3')
+    
+       return Opel.numOfDoors = 4'
+
