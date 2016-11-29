9d813003778e8e7169228d934a6ceccea65ff0ab
Toyeen <rightstiches@yahoo.com>
2016-11-29 18:44:25 +0100

    Add files via upload

---
+++ cmdApp/cmdLineApp.js
@@ -0,0 +1,11 @@
+
+
+//  A command line application to  generate powers of two
+//  from 2^0 through 2^42.
+    
+    for (var val = 1, k = 0; k <= 42; k += 1) {
+
+        console.log('2^%d = %d', i, x);
+
+            val += val;
+    }
