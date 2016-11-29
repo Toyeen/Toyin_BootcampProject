144ba86c4fb52ab6acdf496f1ef37271b0a724d0
Toyeen <rightstiches@yahoo.com>
2016-11-29 15:46:09 +0100

    Add files via upload
    generate prime numbers

---
+++ getPrime/getPrime.js
@@ -0,0 +1,19 @@
+var  getPrime function(num){
+
+	for(i=num;i>0;i--){
+      
+      if(k%2==0){
+    
+      	return;
+
+      	}else{
+
+      	    for (j=arr.length-1;i>0;--i){
+
+               arr[i]
+      	    
+      		   return;
+            }
+        }
+	}
+}
