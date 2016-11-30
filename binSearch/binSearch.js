1151fafd63f91a28775f719d5485a33edbde5fab
Toyeen <rightstiches@yahoo.com>
2016-11-30 16:18:48 +0100

    Add files via upload
    Search for a number in a list using Binary search

---
+++ binSearch/binSearch.js
@@ -0,0 +1,40 @@
+ 
+  var  binSearch: function(Arr[]){
+
+  	        toTwenty:function([1, 2, 3, ....20]){
+
+  	            for(var i == 0; i < toTwenty.length - 1; i++) {
+	
+                  if (toTwenty[i + 1] - toTwenty[i]) == 1;
+
+ 	                 return  toTwenty ([i]);
+            
+            toForty: function(2, 44, 6, 8,......40){
+
+                for(var i = 0; i < toForty.length - 1 ; i++) { 
+
+                    if (toForty[i + 1] - toForty[i]) == 1;
+
+ 	                 return  toForty ([i]);
+
+                       
+             toOneThousand: function(10, 20, 30, 40,......10000){
+
+                for(var i = 0; i < toOneThousand.length - 1 ; i++) { 
+
+                    if (toOneThousand[i + 1] - toOneThousand[i]) == 1;
+
+ 	                    return   toOneThousand ([i]);
+
+                for (var i = 0; i < toOneThousand.length - 1 ; i++) { 
+
+
+
+
+                    return 33
+                    {count: < /* array length */, index: -1} for 33 ', function () {
+
+
+
+                 	)      
+                            
