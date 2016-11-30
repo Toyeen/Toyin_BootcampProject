4fb98ba9856a4b29b549d864f7feb4e21652e8c5
Toyeen <rightstiches@yahoo.com>
2016-11-30 16:15:50 +0100

    Add files via upload
    Find the difference between two arrays

---
+++ findMissing/findMissing.js
@@ -0,0 +1,23 @@
+
+findMissing: function(arr1,arr2){
+
+
+	if ([arr1],[arr2] == 0 && [arr1], [arr2]) == null{
+
+        return 0;
+
+    }else if  (arr1.length == arr2.length){
+
+    	return 0;
+
+    }else if (valueAtIndex(arr1) == valueAtIndex(arr2))) && findMissing(arr1,(arr1)) = diff{
+
+	    return diff;
+
+	}else{
+
+	  	return arr2, arr2;
+
+}
+     
+    
